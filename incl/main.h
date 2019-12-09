79b52bb8fa56c51475270196c0b2bcab93996c2e
RobertoLilloT <roberto.lillo@usach.cl>
2019-12-09 19:28:14 -0300

    Funcionalidad getopt

---
+++ incl/main.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+void usoArgumentos(int numError, int cantError);
---
+++ src/main.c
@@ -0,0 +1,123 @@
+#include "../incl/main.h"
+
+void usoArgumentos(int numError, int cantError)
+{
+	switch (numError)
+	{
+	case 1:
+		printf("La cantidad de argumentos ingresados no es la correcta.\n");
+		break;
+
+	case 2:
+		if (cantError == 1)
+		{
+			printf("%d argumento fue ingresado de forma incorrecta.\n", cantError);
+		}
+		else
+		{
+			printf("%d argumentos fueron ingresados de forma incorrecta.\n", cantError);
+		}
+		break;
+
+	default:
+		break;
+	}
+
+	printf("\nEl formato correcto es el siguiente:\n");
+	printf("\t-c Cantidad de imagenes.\n");
+	printf("\t-m nombre del archivo de mascara.\n");
+	printf("\t-n umbral de negrura.\n");
+	printf("\t-h cantidad de hebras.\n");
+	printf("\t-t tamano del buffer.\n");
+	printf("\t-b bandera para mostrar resultados por pantalla.\n");
+	printf("\n El siguiente corresponde a un ejemplo:\n");
+	printf("\t./lab.out -c 3 -m mascara.txt -n 80 -h 10 -t 10 -b\n");
+
+	return;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc < 11)
+	{
+		usoArgumentos(1, 0);
+	}
+	else if (argc > 12)
+	{
+		usoArgumentos(1, 0);
+	}
+	else
+	{
+		int opcion;
+		int contError = 0;
+
+		int cflag = 0;
+		int mflag = 0;
+		int nflag = 0;
+		int hflag = 0;
+		int tflag = 0;
+		int bflag = 0;
+
+		int cantImagenes, umbNegro, cantHebras, tamBuffer;
+		char *nombreMascara = NULL;
+
+		while ((opcion = getopt(argc, argv, "c:m:n:h:t:b")) != -1)
+		{
+			switch (opcion)
+			{
+			case 'c':
+				cflag = 1;
+				cantImagenes = atoi(optarg);
+				if (cantImagenes == 0)
+				{
+					contError++;
+				}
+				break;
+
+			case 'm':;
+				mflag = 1;
+				nombreMascara = optarg;
+				break;
+
+			case 'n':
+				nflag = 1;
+				umbNegro = atoi(optarg);
+				if (umbNegro == 0)
+				{
+					contError++;
+				}
+				break;
+
+			case 'h':
+				hflag = 1;
+				cantHebras = atoi(optarg);
+				if (cantHebras == 0)
+				{
+					contError++;
+				}
+				break;
+
+			case 't':
+				tflag = 1;
+				tamBuffer = atoi(optarg);
+				if (tamBuffer == 0)
+				{
+					contError++;
+				}
+				break;
+
+			case 'b':
+				bflag = 1;
+				break;
+
+			default:
+				contError++;
+			}
+		}
+		if (contError != 0)
+		{
+			usoArgumentos(2, contError);
+		}
+	}
+	return 0;
+}
